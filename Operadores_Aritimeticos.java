81721ae00fab8538decf39f6f5ba5065539fe057
Henrique <henriquesenah055@gmail.com>
2026-03-06 12:58:53 -0300

    Operadores_Aritimeticos

---
+++ Operadores_Aritimeticos.java
@@ -0,0 +1,18 @@
+public class Operadores_Aritimeticos {
+     public static void main(String[] args) {
+       int y = 6;
+       int x = y++;
+       int a = x + y;
+       System.out.println("x = " + x);
+       System.out.println("y = " + y);
+       System.out.println("x + y = "+ a); 
+    
+       double d = 2.5;
+       d += 2; // d = d + 2 
+       System.out.println(d);
+    }
+
+
+   
+    
+}
