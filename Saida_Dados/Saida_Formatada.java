b4b82b39a319a634e7d77082fd4591a6bb2acfaf
Henrique <henriquesenah055@gmail.com>
2026-03-08 17:30:51 -0300

    Saida_Formatada

---
+++ Saida_Dados/Saida_Formatada.java
@@ -0,0 +1,14 @@
+package Saida_Dados;
+
+public class Saida_Formatada {
+    public static void main(String[] args) {
+     
+     String nome = "Henrique Sena";
+     int idade = 25;
+     String cidade = "São paulo";
+     System.out.println("--------------Dados Pessoais--------------");
+     System.out.printf("%nNome:%s %nIdade:%d %nCidade:%s",nome,idade,cidade);
+    
+    }
+    
+}
