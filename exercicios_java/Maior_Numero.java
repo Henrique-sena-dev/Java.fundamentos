b54d1543d11c82fe5c4fcd6ee7880e816589b5c9
Henrique <henriquesenah055@gmail.com>
2026-03-12 21:56:31 -0300

    exercicios

--- exercicios_java/Maior_Numero.java
+++ exercicios_java/Maior_Numero.java
@@ -7,17 +7,20 @@ public class Maior_Numero {
      Scanner ler = new Scanner(System.in);
      int n1;
      int n2;
-     int res;
      System.out.print("Digite um valor:");
      n1 = ler.nextInt();
      System.out.print("Digite outro valor:");
      n2 = ler.nextInt();
-     res =(n1 > n2)?n1:n2;
-     System.out.println("maior numero:" + res);
+     if(( n1 > n2) && (n1 != n2)){
+        System.out.println(n1 + " É o maior numero e são diferentes!");
+     }
+     else if( n2 > n1){
+        System.out.println(n2 + " É o maior numero e são diferentes!");
+     }
+     else if(n1 == n2){
+        System.out.println("numeros iguais!");
+     }
      ler.close();
    
-
-
- 
     } 
 }
