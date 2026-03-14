4175369a5998d5427493514b0e08b587ec9009d4
Henrique <henriquesenah055@gmail.com>
2026-03-14 00:35:06 -0300

    switch_case

--- d_estruturas_condicionais/Switch_Case.java
+++ d_estruturas_condicionais/Switch_Case.java
@@ -5,25 +5,24 @@ import java.util.Scanner;
 public class Switch_Case {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int salada = 20;
-        int bacon = 22;
-        int egg = 24;
-        int tudo = 30;
-        int burguer = 25;
+        double salada = 20.0;
+        double bacon = 22.0;
+        double egg = 24.0;
+        double burguer = 25.0;
         int opcao;
         System.out.println("==========menu===========");
         System.out.println("   Escolha seu lanche  ");
         System.out.println("1- X-burguer");
         System.out.println("2- X-salada");
-        System.out.println("3- X-bacon"); 
+        System.out.println("3- X-bacon");
         System.out.println("4- X-egg");
-        System.out.println("5- X-tudo");
         System.out.print("Digite um numero de qual opção deseja:");
         opcao = ler.nextInt();
+        ler.close();
         switch (opcao) {
             case 1:
-                System.out.println("Voce escolheu x-burguer valor:" + "R$" + burguer );
-                
+                System.out.println("Voce escolheu x-burguer valor:" + "R$" + burguer);
+
                 break;
             case 2:
                 System.out.println(" Voce escolheu x-salada valor: r$" + salada);
@@ -31,22 +30,19 @@ public class Switch_Case {
                 break;
             case 3:
                 System.out.println("Voce escolheu x-bacon valor: R$" + bacon);
-                
+
                 break;
             case 4:
-                System.out.println(" Voce escolheu x-egg valor: R$" + egg);
+                System.out.println("Voce escolheu x-egg valor: R$" + egg);
 
                 break;
 
             default:
+                System.out.println("opção invalida");
                 break;
+
+             
         }
 
-    
-    
-    
-    
-    
-    
     }
 }
