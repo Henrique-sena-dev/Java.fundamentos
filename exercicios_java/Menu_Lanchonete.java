286b806cab755292d949634e70ffb5ff3aab9719
Henrique <henriquesenah055@gmail.com>
2026-03-15 22:17:07 -0300

    exercicios

---
+++ exercicios_java/Menu_Lanchonete.java
@@ -0,0 +1,47 @@
+package exercicios_java;
+
+import java.util.Scanner;
+
+public class Menu_Lanchonete {
+    public static void main(String[] args) {
+         Scanner ler = new Scanner(System.in);
+        double salada = 20.0;
+        double bacon = 22.0;
+        double egg = 24.0;
+        double burguer = 25.0;
+        int opcao;
+        System.out.println("==========menu===========");
+        System.out.println("   Escolha seu lanche  ");
+        System.out.println("1- X-burguer");
+        System.out.println("2- X-salada");
+        System.out.println("3- X-bacon");
+        System.out.println("4- X-egg");
+        System.out.print("Digite um numero de qual opção deseja:");
+        opcao = ler.nextInt();
+        ler.close();
+        switch (opcao) {
+            case 1:
+                System.out.println("Voce escolheu x-burguer valor:" + "R$" + burguer);
+
+                break;
+            case 2:
+                System.out.println(" Voce escolheu x-salada valor: r$" + salada);
+
+                break;
+            case 3:
+                System.out.println("Voce escolheu x-bacon valor: R$" + bacon);
+
+                break;
+            case 4:
+                System.out.println("Voce escolheu x-egg valor: R$" + egg);
+
+                break;
+
+            default:
+                System.out.println("opção invalida");
+                break;
+
+             
+        }
+    }
+}
